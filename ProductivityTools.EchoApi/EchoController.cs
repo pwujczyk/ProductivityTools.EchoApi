55ad7728c298e6166c11081cf7fef952196afc45
Pawel Wujczyk <pwujczyk@hotmail.com>
2021-06-03 13:08:39 +0200

    initial commit

---
+++ ProductivityTools.EchoApi/EchoController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+
+namespace ProductivityTools.EchoApi
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class EchoController : Controller
+    {
+        [HttpGet]
+        [Route("Date")]
+        public string Date()
+        {
+            return DateTime.Now.ToString();
+        }
+
+        [HttpPost]
+        [Route("Hello")]
+        public string Hello(object s)
+        {
+            return string.Concat($"Hello {s.ToString()} Current date:{DateTime.Now}");
+        }
+    }
+}
